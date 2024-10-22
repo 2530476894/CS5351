7c7cb3ab69cced0d1686059f100322fa1522b258
TheTurtle2020 <almostsir2022@gmail.com>
2024-10-22 10:43:37 +0800

    feature: query && page

--- scrumflow/src/main/java/com/se/scrumflow/service/ItemService.java
+++ scrumflow/src/main/java/com/se/scrumflow/service/ItemService.java
@@ -1,9 +1,16 @@
 package com.se.scrumflow.service;
 
 import com.se.scrumflow.dto.req.ItemCreateReqDTO;
+import com.se.scrumflow.dto.req.ItemPageReqDTO;
+import com.se.scrumflow.dto.resp.ItemPageRespDTO;
+import com.se.scrumflow.dto.resp.ItemQueryRespDTO;
+import com.se.scrumflow.utils.Page;
 
 public interface ItemService {
 
     void createItem(ItemCreateReqDTO requestParam);
 
+    ItemQueryRespDTO queryItem(String id);
+
+    Page<ItemPageRespDTO> pageItem(ItemPageReqDTO requestParam);
 }
