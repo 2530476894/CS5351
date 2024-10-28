b26506daf9254b01fc96c23ccba878ecbbee91e7
liurunxuan <13960777+liurunxuan@user.noreply.gitee.com>
2024-10-29 00:16:02 +0800

    燃尽图分割切口，时间区间处理

--- scrumflow/src/main/java/com/se/scrumflow/service/BurndownService.java
+++ scrumflow/src/main/java/com/se/scrumflow/service/BurndownService.java
@@ -4,5 +4,10 @@ import com.se.scrumflow.dao.entity.BurndownDO;
 import org.bson.types.ObjectId;
 
 public interface BurndownService {
-    BurndownDO createOrUpdateAndGetBurndownData(ObjectId sprintId);
+    BurndownDO createBurndown(ObjectId sprintId);
+
+    BurndownDO initBurndown(ObjectId sprintId);
+
+    BurndownDO updateBurndown(ObjectId sprintId);
+
 }
--- scrumflow/src/main/java/com/se/scrumflow/service/impl/BurndownServiceImpl.java
+++ scrumflow/src/main/java/com/se/scrumflow/service/impl/BurndownServiceImpl.java
@@ -1,10 +1,10 @@
 package com.se.scrumflow.service.impl;
 
+import cn.hutool.core.date.DateUtil;
 import com.se.scrumflow.dao.entity.BurndownDO;
 import com.se.scrumflow.dao.entity.ItemDO;
 import com.se.scrumflow.dao.entity.SprintDO;
 import com.se.scrumflow.dao.repository.BurndownRepository;
-import com.se.scrumflow.dao.repository.ItemRepository;
 import com.se.scrumflow.dao.repository.SprintRepository;
 import com.se.scrumflow.dto.req.ActualRemainingStoryPointDTO;
 import com.se.scrumflow.dto.req.PlannedRemainingStoryPointDTO;
@@ -17,28 +17,30 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-
+import java.util.Optional;
 
 @Service
 public class BurndownServiceImpl implements BurndownService {
-@Autowired
-private BurndownRepository burndownRepository;
-@Autowired
-private MongoTemplate mongoTemplate;
-@Autowired
-private SprintRepository sprintRepository;
+
+    @Autowired
+    private BurndownRepository burndownRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Autowired
+    private SprintRepository sprintRepository;
 
     @Override
-    public BurndownDO createOrUpdateAndGetBurndownData(ObjectId sprintId) {
+    public BurndownDO createBurndown(ObjectId sprintId) {
         BurndownDO burndownDO = burndownRepository.findBySprintId(sprintId);
         if (burndownDO == null) {
-            SprintDO sprint = sprintRepository.findById(sprintId).orElse(null);
-            if (sprint!= null) {
-                Date startTime = sprint.getStartDate();
-                Date endTime = sprint.getEndDate();
+            Optional<SprintDO> optionalSprint = sprintRepository.findById(sprintId);
+            if (optionalSprint.isPresent()) {
+                SprintDO sprint = optionalSprint.get();
+                // 获取冲刺的开始时间和结束时间
+                Date startDate = sprint.getStartDate();
+                Date endDate = sprint.getEndDate();
                 // 构建查询条件，根据sprintId查询对应的Items
                 Query query = new Query(Criteria.where("sprintId").is(sprintId));
                 // 使用MongoTemplate执行查询，获取对应的Items列表
@@ -50,8 +52,8 @@ private SprintRepository sprintRepository;
                 }
                 BurndownDO newBurndownDO = new BurndownDO();
                 newBurndownDO.setSprintId(sprintId);
-                newBurndownDO.setStartTime(startTime);
-                newBurndownDO.setEndTime(endTime);
+                newBurndownDO.setStartTime(startDate);
+                newBurndownDO.setEndTime(endDate);
                 newBurndownDO.setTotalStoryPoints(totalStoryPoints);
                 burndownRepository.save(newBurndownDO);
                 return newBurndownDO;
@@ -60,126 +62,89 @@ private SprintRepository sprintRepository;
                 throw new IllegalArgumentException("找不到对应的SprintDO，sprintId: " + sprintId);
             }
         } else {
-            // 更新burndownDO
-            SprintDO sprint = sprintRepository.findById(sprintId).orElse(null);
-            if (sprint!= null) {
-                // 检查并更新时间属性
-                if (sprint.getStartDate()!= null &&!sprint.getStartDate().equals(burndownDO.getStartTime())) {
-                    burndownDO.setStartTime(sprint.getStartDate());
-                }
-                if (sprint.getEndDate()!= null &&!sprint.getEndDate().equals(burndownDO.getEndTime())) {
-                    burndownDO.setEndTime(sprint.getEndDate());
-                }
-                // 重新计算总故事点
-                Query query = new Query(Criteria.where("sprintId").is(sprintId));
-                List<ItemDO> items = mongoTemplate.find(query, ItemDO.class);
-                int newTotalStoryPoints = 0;
-                for (ItemDO item : items) {
-                    newTotalStoryPoints += item.getStoryPoint();
-                }
-                burndownDO.setTotalStoryPoints(newTotalStoryPoints);
-                // 计算并更新actualRemainingStoryPoints
-                List<ActualRemainingStoryPointDTO> actualRemainingStoryPoints = calculateActualRemainingStoryPoints(sprintId,burndownDO);
-                burndownDO.setActualRemainingStoryPoints(actualRemainingStoryPoints);
-
-                // 计算并更新plannedRemainingStoryPoints
-                List<PlannedRemainingStoryPointDTO> plannedRemainingStoryPoints = calculatePlannedRemainingStoryPoints(sprintId,burndownDO);
-                burndownDO.setPlannedRemainingStoryPoints(plannedRemainingStoryPoints);
-                burndownRepository.save(burndownDO);
-            } else {
-                throw new IllegalArgumentException("找不到对应的SprintDO，sprintId: " + sprintId);
-            }
             return burndownDO;
         }
     }
 
-    private List<PlannedRemainingStoryPointDTO> calculatePlannedRemainingStoryPoints(ObjectId sprintId,BurndownDO burndownDO) {
+    @Override
+    public BurndownDO initBurndown(ObjectId sprintId) {
+        BurndownDO burndownDO = burndownRepository.findBySprintId(sprintId);
+        if (burndownDO == null) {
+            throw new IllegalArgumentException("找不到对应的BurndownDO，sprintId: " + sprintId);
+        }
         List<PlannedRemainingStoryPointDTO> plannedRemainingStoryPointsList = new ArrayList<>();
         // 获取冲刺的开始时间和结束时间
-        SprintDO sprint = mongoTemplate.findById(sprintId, SprintDO.class);
-        Date startDate = sprint.getStartDate();
-        Date endDate = sprint.getEndDate();
-        // 将开始时间和结束时间设置为当天的起始和结束时刻，以便后续比较
-        startDate = setToStartOfDay(startDate);
-        endDate = setToEndOfDay(endDate);
-        // 循环遍历每一天
-        while (!startDate.after(endDate)) {
-            // 查询计划中当天应该完成的ItemDO
-            Query queryItems = new Query(Criteria
-                    .where("sprintId").is(sprintId)
-                    .and("plannedEndTime").lte(endDate).gte(startDate));
+        Optional<SprintDO> optionalSprint = Optional.ofNullable(mongoTemplate.findById(sprintId, SprintDO.class));
+        if (optionalSprint.isPresent()) {
+            SprintDO sprint = optionalSprint.get();
+            Date startDate = sprint.getStartDate();
+            Date endDate = sprint.getEndDate();
+            // 将开始时间和结束时间设置为当天的起始和结束时刻
+            startDate = DateUtil.beginOfDay(startDate);
+            endDate = DateUtil.endOfDay(endDate);
+            // 循环遍历每一天
+            for (Date currentDate = startDate;!currentDate.after(endDate); currentDate = DateUtil.offsetDay(currentDate, 1)) {
+                // 查询计划中当天应该完成的 ItemDO
+                Query queryItems = new Query(Criteria
+                        .where("sprintId").is(sprintId)
+                        .and("plannedEndTime").lte(currentDate).gte(currentDate));
 
-            List<ItemDO> plannedItems = mongoTemplate.find(queryItems, ItemDO.class);
-            // 计算当天计划剩余的故事点
-            int plannedRemaining = burndownDO.getTotalStoryPoints();
-            for (ItemDO item : plannedItems) {
-                plannedRemaining -= item.getStoryPoint();
+                List<ItemDO> plannedItems = mongoTemplate.find(queryItems, ItemDO.class);
+                // 计算当天计划剩余的故事点
+                int plannedRemaining = burndownDO.getTotalStoryPoints();
+                for (ItemDO item : plannedItems) {
+                    plannedRemaining -= item.getStoryPoint();
+                }
+                // 创建 PlannedRemainingStoryPointDTO 对象并添加到列表中
+                PlannedRemainingStoryPointDTO dto = new PlannedRemainingStoryPointDTO(currentDate, plannedRemaining);
+                plannedRemainingStoryPointsList.add(dto);
             }
-            // 创建PlannedRemainingStoryPointDTO对象并添加到列表中
-            PlannedRemainingStoryPointDTO dto = new PlannedRemainingStoryPointDTO(startDate, plannedRemaining);
-            plannedRemainingStoryPointsList.add(dto);
-            // 将日期推进一天
-            startDate = addOneDay(startDate);
+            burndownDO.setPlannedRemainingStoryPoints(plannedRemainingStoryPointsList);
+            burndownRepository.save(burndownDO);
+            return burndownDO;
+        } else {
+            throw new IllegalArgumentException("找不到对应的SprintDO，sprintId: " + sprintId);
         }
-        return plannedRemainingStoryPointsList;
     }
 
-    private List<ActualRemainingStoryPointDTO> calculateActualRemainingStoryPoints(ObjectId sprintId,BurndownDO burndownDO) {
-        List<ActualRemainingStoryPointDTO> actualRemainingStoryPointsList = new ArrayList<>();
+    @Override
+    public BurndownDO updateBurndown(ObjectId sprintId) {
+        BurndownDO burndownDO = burndownRepository.findBySprintId(sprintId);
+        if (burndownDO == null) {
+            throw new IllegalArgumentException("找不到对应的BurndownDO，sprintId: " + sprintId);
+        }
+        List<ActualRemainingStoryPointDTO> actualRemainingStoryPointDTOS = new ArrayList<>();
         // 获取冲刺的开始时间和结束时间
-        SprintDO sprint = mongoTemplate.findById(sprintId, SprintDO.class);
-        Date startDate = sprint.getStartDate();
-        Date endDate = sprint.getEndDate();
-        // 将开始时间和结束时间设置为当天的起始和结束时刻，以便后续比较
-        startDate = setToStartOfDay(startDate);
-        endDate = setToEndOfDay(endDate);
-        // 循环遍历每一天
-        while (!startDate.after(endDate)) {
-            // 查询当天状态为已完成的ItemDO
-            Query queryItems = new Query(Criteria
-                    .where("sprintId").is(sprintId)
-                    .and("status").is(1) // 假设1表示已完成状态，根据实际情况调整
-                    .and("endTime").lte(endDate).gte(startDate));
-            List<ItemDO> completedItems = mongoTemplate.find(queryItems, ItemDO.class);
-            // 计算当天实际剩余的故事点
-            int actualRemaining = burndownDO.getTotalStoryPoints();
-            for (ItemDO item : completedItems) {
-                actualRemaining -= item.getStoryPoint();
+        Optional<SprintDO> optionalSprint = Optional.ofNullable(mongoTemplate.findById(sprintId, SprintDO.class));
+        if (optionalSprint.isPresent()) {
+            SprintDO sprint = optionalSprint.get();
+            Date startDate = sprint.getStartDate();
+            Date endDate = sprint.getEndDate();
+            // 将开始时间和结束时间设置为当天的起始和结束时刻
+            startDate = DateUtil.beginOfDay(startDate);
+            endDate = DateUtil.endOfDay(endDate);
+            // 循环遍历每一天
+            for (Date currentDate = startDate;!currentDate.after(endDate); currentDate = DateUtil.offsetDay(currentDate, 1)) {
+                // 查询计划中当天应该完成的 ItemDO
+                Query queryItems = new Query(Criteria
+                        .where("sprintId").is(sprintId)
+                        .and("status").is(1));
+
+                List<ItemDO> plannedItems = mongoTemplate.find(queryItems, ItemDO.class);
+                // 计算当天计划剩余的故事点
+                int actualRemaining = burndownDO.getTotalStoryPoints();
+                for (ItemDO item : plannedItems) {
+                    actualRemaining -= item.getStoryPoint();
+                }
+                // 创建 PlannedRemainingStoryPointDTO 对象并添加到列表中
+                ActualRemainingStoryPointDTO dto = new ActualRemainingStoryPointDTO(currentDate, actualRemaining);
+                actualRemainingStoryPointDTOS.add(dto);
             }
-            // 创建ActualRemainingStoryPointDTO对象并添加到列表中
-            ActualRemainingStoryPointDTO dto = new ActualRemainingStoryPointDTO(startDate, actualRemaining);
-            actualRemainingStoryPointsList.add(dto);
-            // 将日期推进一天
-            startDate = addOneDay(startDate);
+            burndownDO.setActualRemainingStoryPoints(actualRemainingStoryPointDTOS);
+            burndownRepository.save(burndownDO);
+            return burndownDO;
+        } else {
+            throw new IllegalArgumentException("找不到对应的SprintDO，sprintId: " + sprintId);
         }
-        return actualRemainingStoryPointsList;
-    }
-
-
-    private Date setToStartOfDay(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTime();
-    }
-    // 将日期设置为当天的结束时刻（23:59:59）
-    private Date setToEndOfDay(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 23);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
-        return calendar.getTime();
-    }
-    // 将日期推进一天
-    private Date addOneDay(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.DAY_OF_MONTH, 1);
-        return calendar.getTime();
     }
 }
