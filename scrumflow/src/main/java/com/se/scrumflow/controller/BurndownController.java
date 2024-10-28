f12b953ce39c2cf4a3dd3f02bb5c66c274bad4ad
liurunxuan <13960777+liurunxuan@user.noreply.gitee.com>
2024-10-29 00:15:05 +0800

    燃尽图数据操控层

--- scrumflow/src/main/java/com/se/scrumflow/controller/BurndownController.java
+++ scrumflow/src/main/java/com/se/scrumflow/controller/BurndownController.java
@@ -8,16 +8,29 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
-@RequestMapping("/api/burndown")
+@RequestMapping("/burndown")
 public class BurndownController {
 
     @Autowired
     private BurndownService burndownService;
 
-    @GetMapping("/{sprintId}")
-    public Result<BurndownDO> getBurndownData(@PathVariable("sprintId") String sprintId) {
-       BurndownDO burndownData = burndownService.createOrUpdateAndGetBurndownData(new ObjectId(sprintId));
-       return Results.success(burndownData);
+    @PostMapping("/{sprintId}")
+    public Result<BurndownDO> createBurndown(@PathVariable ObjectId sprintId) {
+        BurndownDO burndown = burndownService.createBurndown(sprintId);
+        return Results.success(burndown);
+    }
+
+    @GetMapping("/init/{sprintId}")
+    public Result<BurndownDO> initBurndown(@PathVariable ObjectId sprintId) {
+        BurndownDO burndown = burndownService.initBurndown(sprintId);
+        return Results.success(burndown);
+    }
+
+    @PutMapping("/update/{sprintId}")
+    public Result<BurndownDO> updateBurndown(@PathVariable ObjectId sprintId) {
+        BurndownDO burndown = burndownService.updateBurndown(sprintId);
+        return Results.success(burndown);
     }
 }
