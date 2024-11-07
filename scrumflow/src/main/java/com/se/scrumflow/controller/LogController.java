4d03185e1778e1829f25ab680f1481c8faf6c10c
TheTurtle2020 <almostsir2022@gmail.com>
2024-11-07 18:23:17 +0800

    feature: page-query logs of certain item.

--- scrumflow/src/main/java/com/se/scrumflow/controller/LogController.java
+++ scrumflow/src/main/java/com/se/scrumflow/controller/LogController.java
@@ -3,12 +3,12 @@ package com.se.scrumflow.controller;
 import com.se.scrumflow.common.convention.result.Result;
 import com.se.scrumflow.common.convention.result.Results;
 import com.se.scrumflow.dto.req.LogCreateReqDTO;
+import com.se.scrumflow.dto.req.LogPageReqDTO;
+import com.se.scrumflow.dto.resp.LogPageRespDTO;
 import com.se.scrumflow.service.LogService;
+import com.se.scrumflow.utils.Page;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/log")
@@ -21,6 +21,11 @@ public class LogController {
     public Result<Void> createLog(@RequestBody LogCreateReqDTO requestParam) {
         logService.createLog(requestParam);
         return Results.success();
+    }
+
+    @GetMapping("/page")
+    public Result<Page<LogPageRespDTO>> pageLog(@RequestBody LogPageReqDTO requestParam) {
+        return Results.success(logService.pageLog(requestParam));
     }
 
 }
--- scrumflow/src/main/java/com/se/scrumflow/service/impl/LogServiceImpl.java
+++ scrumflow/src/main/java/com/se/scrumflow/service/impl/LogServiceImpl.java
@@ -4,9 +4,18 @@ import cn.hutool.core.bean.BeanUtil;
 import com.se.scrumflow.dao.entity.LogDO;
 import com.se.scrumflow.dao.repository.LogRepository;
 import com.se.scrumflow.dto.req.LogCreateReqDTO;
+import com.se.scrumflow.dto.req.LogPageReqDTO;
+import com.se.scrumflow.dto.resp.LogPageRespDTO;
 import com.se.scrumflow.service.LogService;
+import com.se.scrumflow.utils.GeneralOperations;
+import com.se.scrumflow.utils.Page;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +23,23 @@ public class LogServiceImpl implements LogService {
 
     private final LogRepository logRepository;
 
+    private final MongoTemplate mongoTemplate;
+
     @Override
     public void createLog(LogCreateReqDTO requestParam) {
         LogDO logDO = BeanUtil.copyProperties(requestParam, LogDO.class);
         logRepository.insert(logDO);
+    }
+
+    @Override
+    public Page<LogPageRespDTO> pageLog(LogPageReqDTO requestParam) {
+        Query query = GeneralOperations.buildQueryOrUpdate(requestParam, Query.class);
+        query.with(PageRequest.of(requestParam.getPageNumber(), requestParam.getPageSize()));
+        List<LogDO> logDOList = mongoTemplate.find(query, LogDO.class);
+        List<LogPageRespDTO> logPageRespDTOList = logDOList.stream()
+                .map(logDO -> BeanUtil.copyProperties(logDO, LogPageRespDTO.class))
+                .toList();
+        return Page.create(logPageRespDTOList, logPageRespDTOList.size());
     }
 
 }
